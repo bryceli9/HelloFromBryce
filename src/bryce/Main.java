b814dd0efe298f88275dc3f3a88f0756a08ec4a4
FerracciuO <alv20031@byui.edu>
2021-05-22 18:31:49 -0300

    Hello my friend, I took the liberty of add a new line to be print out on your code.

--- src/bryce/Main.java
+++ src/bryce/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("My name is Bryce li");
+	    System.out.println("Hello Bryce, my name is Oscar.");
     }
+
 }
